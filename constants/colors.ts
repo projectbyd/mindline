50ce74ca45ed93dc8a3bdc76d4b2a5908aa4a7c8
projectbyd <project.by.deee@gmail.com>
2026-05-17 20:27:17 +0700

    Create colors.ts

---
+++ constants/colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  primaryPurple: '#5758A6',
+  softYellow: '#E7C58A',
+  softGreen: '#86B05D',
+  softRed: '#C77974',
+  softCream: '#F7E6D4',
+  darkBrown: '#4B3535',
+
+  background: '#F7E6D4',
+  card: '#FFF8F2',
+  textPrimary: '#4B3535',
+  textSecondary: '#7A6A6A',
+  border: '#E8D8C8',
+};
